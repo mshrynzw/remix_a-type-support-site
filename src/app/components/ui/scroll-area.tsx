11e70fdb0683b41ab065a27147f56be2862ce39d
mshrynzw <iq87io25@gmail.com>
2026-03-21 20:32:17 +0900

    Edit: Scrollbar

--- src/app/components/ui/scroll-area.tsx
+++ src/app/components/ui/scroll-area.tsx
@@ -49,7 +49,7 @@ function ScrollBar({
     >
       <ScrollAreaPrimitive.ScrollAreaThumb
         data-slot="scroll-area-thumb"
-        className="bg-border relative flex-1 rounded-full"
+        className="relative flex-1 rounded-full bg-pink-300/45 transition-colors hover:bg-pink-400/60"
       />
     </ScrollAreaPrimitive.ScrollAreaScrollbar>
   );
